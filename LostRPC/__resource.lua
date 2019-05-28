b91511cb73ec1b0f48acf1e410e89b817b4d5f34
JI <49317513+LezZJIM@users.noreply.github.com>
2019-05-28 16:06:29 +0300

    Add files via upload

---
+++ LostRPC/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+
+client_script 'fingerc.lua'
+client_script "shuffc.lua"
+client_script "Nodrivec.lua"
+client_script 'discordarayuzc.lua'
+client_script 'vehcontrolc.lua'
+server_script 'vehcontrols.lua'
---
+++ LostRPC/discordarayuzc.lua
@@ -0,0 +1,23 @@
+Citizen.CreateThread(function()
+	while true do
+        --This is the Application ID (Replace this with you own)
+		SetDiscordAppId(581122080763019291)
+
+        --Here you will have to put the image name for the "large" icon.
+		SetDiscordRichPresenceAsset('logo_name')
+        
+        --(11-11-2018) New Natives:
+
+        --Here you can add hover text for the "large" icon.
+        SetDiscordRichPresenceAssetText('Lost RolePlay Oynuyor')
+       
+        --Here you will have to put the image name for the "small" icon.
+        SetDiscordRichPresenceAssetSmall('logo_name')
+
+        --Here you can add hover text for the "small" icon.
+        SetDiscordRichPresenceAssetSmallText('Lost RolePlay de Rol Yapıyor')
+
+        --It updates every one minute just in case.
+		Citizen.Wait(0)
+	end
+end)
